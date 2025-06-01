f519c5b66c0ec3d3c682344e2d2ee4b4a4046227
Laurence Trippen <laurence.trippen@gmail.com>
2025-06-01 13:34:08 +0400

    added subpages

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-
-import pedometer from "./experiments/pedometer";
-
-pedometer();
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from "vite";
+import path from "path"
+import url from "url"
+
+import { defineConfig } from "vite"
+
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+
 
 export default defineConfig({
   base: "/vite-webview-sandbox",
+  build: {
+    rollupOptions: {
+      main: path.resolve(__dirname, "index.html"),
+      tts: path.resolve(__dirname, "tts.html"),
+      blob: path.resolve(__dirname, "blob.html"),
+    },
+  },
 })
